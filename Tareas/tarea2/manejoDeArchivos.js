96fae4f3fa62a8aac939a67ecb7be9ef502efdfb
santiago sierra <santiscano@gmail.com>
2022-10-12 23:36:30 -0500

    tarea3

--- Tareas/tarea2/manejoDeArchivos.js
+++ Tareas/tarea2/manejoDeArchivos.js
@@ -4,31 +4,37 @@ const path = require('path');
 class Contenedor {
     
     constructor(nameFile){
-
+        // ubicacion archivo a manipular con los metodos
         this.nameFile =`./${nameFile}.json`;       
     }
 
+    //leer archivo 
     async getJson(){
+        // almacena laa info del archivo en esta variable
         const data = await fs.promises.readFile(this.nameFile,'utf-8');
+        // retorna objeto json
         return JSON.parse(data);
     }
 
+    // actualizar
     async updataFile(products){
         fs.promises.writeFile(this.nameFile, JSON.stringify(products, null, '\t'), 'utf-8');
     }
 
+    // leer todo
     async getAll() {
         const data =  await this.getJson();
         return data;
     };
 
+    // metodo 1
     async save(data){
         if(!data.title || !data.price || typeof data.title !== 'string' || typeof data.price !== 'number') throw new Error('verificar, Datos invalidos');
         let id = 1;
         const products =   await this.getJson();
-       if(products.length){
-          id = products[products.length -1].id +1;
-       }
+        if(products.length){
+            id = products[products.length -1].id +1;
+        }
         const newProduct = {
             title: data.title,
             price: data.price,
@@ -87,7 +93,7 @@ const fileSintax = async() =>{
 
     const getAll2 = await products.getAll();
     console.log(getAll2);
-  
+
 }
 
 fileSintax();
---
+++ Tareas/tarea3/express.js
@@ -0,0 +1,46 @@
+// 1 puerto 8080
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const puerto = 8080;
+const server = app.listen(puerto,()=>{
+    console.log('escuchando en ',puerto)
+});
+server.on('error',(err)=>{console.log(err)});
+
+// referencio la ubicacion del archivo
+const productos = path.resolve(__dirname,'./productos.txt')
+
+// a- get '/productos' devuelve array con todos los productos
+app.get('/productos',(req,res)=>{
+    res.json(
+        fs.readFile(productos, 'utf8', (err, data)=>{
+            if(err) throw err;
+            console.log(data);
+        })
+    )
+})
+// b- get '/productoRandom devuelve un producto al azar
+const between = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+    };
+app.get('/productoRandom',(req, res)=>{
+    res.json(
+        fs.readFile(productos, 'utf-8',(err, data)=>{
+            if(err) throw err;
+            // valido si lee el archivo
+            console.log(productos)
+            //  ejecuto funcion between
+            console.log(`el producto elegido es ${between(1, productos.products.length)}`);
+        })
+    )
+})
+
+
+
+
+
+
+
